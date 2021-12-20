2c9a3fd888a5b286b4f2e7005ad5744397b071eb
dustinturp <dustinturp@outlook.com>
2021-12-19 20:27:18 -0600

    inital commit added indexDB for offline use

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const APP_PREFIX = 'FoodEvent-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION
+const FILES_TO_CACHE = [
+    "./index.html",
+    "",
+    "",
+    "",
+];
+
